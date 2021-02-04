00cc5f5bbc6c2568f3d5d9ae17d9f11de85b13c0
whoami <whoami@whoamis-MBP.T-mobile.com>
2021-02-04 10:42:09 -0600

    updage build db data ingestion part

--- Json_fetch_build.py
+++ Json_fetch_build.py
@@ -11,30 +11,32 @@ import json
 source_url = 'https://raw.githubusercontent.com/ctiml/convenience-store-data/master/7-11/台北市.json'
 PASSWORD = '1234'
 DBNAME = 'taipei_seven'
+COLLECTION_NAME = 'taipei_seven'
 mongo_server_url = "mongodb+srv://Admin:"+PASSWORD+"@cluster0.owy49.mongodb.net/"+DBNAME+"?retryWrites=true&w=majority"
 
 # logging configuration
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
 
-def data_fetch(self, source_url):
+def data_fetch(source_url):
     try:
         r= requests.get(source_url)
         if r.status_code == 200:
             logger.info("Successful fetch data from source.")
             return r.json()
-        else: 
-            logger.error("Endpoint returned failed status.\n")
-            logger.info("Status for fetching source data: {}".format(r.text))
+    except:
+        logger.error("Endpoint returned failed status.\n")
+        logger.info("Status for fetching source data: {}".format(r.text))
 
-def data_screen(self, data):
+
+def data_screen(data):
     IDs =[]
     for item in data:
         IDs+=[item['POIID']]
-    
-    logger.info("total IDs:", len(data), "unique IDs:", len(set(IDs)))
+    logger.info("total IDs: {}".format(len(data)))
+    logger.info("unique IDs: {}".format(len(set(IDs))))
 
-def parse_source_json(self, data):
+def parse_source_json(data):
 
     """ This function parses the JSON content and leave only the features needed,
         Set the location coordinates right for mongo ingestion.
@@ -57,22 +59,23 @@ def parse_source_json(self, data):
         data_dict.append(tmp_dict)
     return data_dict
 
-def mongo_connect(self,mongo_server_url, DBNAME, COLLECTION_NAME):
+def mongo_connect(mongo_server_url, dbname, collection_name):
     try:
-        cluster = pymongo.MongoClient(client_url)
+        cluster = pymongo.MongoClient(mongo_server_url)
         cluster.server_info()
-        db=cluster[DBNAME]
-        collection = db[COLLECTION_NAME]
-        except pymongo.errors.ServerSelectionTimeoutError as err:
-            logger.error(err)
-        except pymongo.errors.ConnectionFailure as err:
-            logger.error(err)
-        except pymongo.errors.InvalidURI as err:
-            logger.error(err)
-        except pymongo.errors.OperationFailure as err:
-            logger.error(err)
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        logger.error(err)
+    except pymongo.errors.ConnectionFailure as err:
+        logger.error(err)
+    except pymongo.errors.InvalidURI as err:
+        logger.error(err)
+    except pymongo.errors.OperationFailure as err:
+        logger.error(err)
     except:
         logger.error("connection error. Unknown issue.")
+
+    db=cluster[dbname]
+    collection = db[collection_name]
 
     return collection
 
@@ -85,14 +88,17 @@ if __name__ == "__main__":
     data = parse_source_json(data)
 
     # initiate connection to mongo
-    collection = mongo_connect(mongo_server_url, DBNAME, DBNAME)
+    collection = mongo_connect(mongo_server_url, DBNAME, COLLECTION_NAME)
 
     # ingest data
     for doc in data:
-        try:
+        try:  # could implement input data schema checks, e.g. validation_schema = {"$jsonSchema":{... , when multiple sources of input 
             result = collection.insert_one(doc)
             if result.inserted_id:
                 logger.info("successful insert, document: {}".format(result.inserted_id))
+        except pymongo.errors.DuplicateKeyError as err:  # skip dupped key(_id)
+            logger.error("Document id duplicated, skip inserting...: {}".format(err['errmsg']))
+            continue
         except Excpetion as err:
             logger.error("An exception occurred: {}".format(err))
             
